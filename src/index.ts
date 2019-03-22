595c6a421c0049ebea692a3f443b5b830a5d8f54
Jamie Birch work laptop <jbizzle5@gmail.com>
2019-03-22 09:42:21 +0000

    All HostConfig stubs added.

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,120 @@ type UpdatePayload = any;
 type ChildSet = any;
 type TimeoutHandle = any;
 type NoTimeout = any;
+const noTimeoutValue: NoTimeout = undefined;
 
 // https://medium.com/@agent_hunt/hello-world-custom-react-renderer-9a95b7cd04bc
 const hostConfig: ReactReconciler.HostConfig<Type, Props, Container, Instance, TextInstance, HydratableInstance, PublicInstance, HostContext, UpdatePayload, ChildSet, TimeoutHandle, NoTimeout> = {
+    getPublicInstance(instance: Instance | TextInstance): PublicInstance {
+
+    },
+    getRootHostContext(rootContainerInstance: Container): HostContext {
+
+    },
+    getChildHostContext(parentHostContext: HostContext, type: Type, rootContainerInstance: Container): HostContext {
+
+    },
+    prepareForCommit(containerInfo: Container): void {
+
+    },
+    resetAfterCommit(containerInfo: Container): void {
+
+    },
+    createInstance(
+        type: Type,
+        props: Props,
+        rootContainerInstance: Container,
+        hostContext: HostContext,
+        internalInstanceHandle: ReactReconciler.OpaqueHandle,
+    ): Instance {
+
+    },
+    appendInitialChild(parentInstance: Instance, child: Instance | TextInstance): void {
+
+    },
+    finalizeInitialChildren(
+        parentInstance: Instance,
+        type: Type,
+        props: Props,
+        rootContainerInstance: Container,
+        hostContext: HostContext,
+    ): boolean {
+
+    },
+    prepareUpdate(
+        instance: Instance,
+        type: Type,
+        oldProps: Props,
+        newProps: Props,
+        rootContainerInstance: Container,
+        hostContext: HostContext,
+    ): null | UpdatePayload {
+
+    },
+    shouldSetTextContent(type: Type, props: Props): boolean {
+
+    },
+    shouldDeprioritizeSubtree(type: Type, props: Props): boolean {
+
+    },
+    createTextInstance(
+        text: string,
+        rootContainerInstance: Container,
+        hostContext: HostContext,
+        internalInstanceHandle: ReactReconciler.OpaqueHandle,
+    ): TextInstance {
+
+    },
+    scheduleDeferredCallback(
+        callback: () => any,
+        options?: { timeout: number },
+    ): any {
+
+    },
+    cancelDeferredCallback(callbackID: any): void {
+
+    },
+    setTimeout(handler: (...args: any[]) => void, timeout: number): TimeoutHandle | NoTimeout {
+
+    },
+    clearTimeout(handle: TimeoutHandle | NoTimeout): void {
+
+    },
+    noTimeout: noTimeoutValue,
     now: Date.now,
-};
+    isPrimaryRenderer: true,
+    supportsMutation: true, // TODO
+    supportsPersistence: false,
+    supportsHydration: false,
+
+    /* Mutation (optional) */
+    appendChild(parentInstance: Instance, child: Instance | TextInstance): void {},
+    appendChildToContainer(container: Container, child: Instance | TextInstance): void {},
+    commitTextUpdate(textInstance: TextInstance, oldText: string, newText: string): void {},
+    commitMount(
+        instance: Instance,
+        type: Type,
+        newProps: Props,
+        internalInstanceHandle: ReactReconciler.OpaqueHandle,
+    ): void {},
+    commitUpdate(
+        instance: Instance,
+        updatePayload: UpdatePayload,
+        type: Type,
+        oldProps: Props,
+        newProps: Props,
+        internalInstanceHandle: ReactReconciler.OpaqueHandle,
+    ): void {},
+    insertBefore(parentInstance: Instance, child: Instance | TextInstance, beforeChild: Instance | TextInstance): void {},
+    insertInContainerBefore(
+        container: Container,
+        child: Instance | TextInstance,
+        beforeChild: Instance | TextInstance,
+    ): void {},
+    removeChild(parentInstance: Instance, child: Instance | TextInstance): void {},
+    removeChildFromContainer(container: Container, child: Instance | TextInstance): void {},
+    resetTextContent(instance: Instance): void {},
+}
 const ReactReconcilerInst = ReactReconciler<Type, Props, Container, Instance, TextInstance, HydratableInstance, PublicInstance, HostContext, UpdatePayload, ChildSet, TimeoutHandle, NoTimeout>(hostConfig);
 
 export default {
