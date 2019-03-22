f52dea20d6b8fdfbf9b4f1b81abc15923f74bdbf
Jamie Birch work laptop <jbizzle5@gmail.com>
2019-03-22 09:32:55 +0000

    Typings!

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,42 @@
 import ReactReconciler from 'react-reconciler';
 
-const hostConfig = {
+type Type = any;
+type Props = any;
+type Container = any;
+type Instance = any;
+type TextInstance = any;
+type HydratableInstance = any;
+type PublicInstance = any;
+type HostContext = any;
+type UpdatePayload = any;
+type ChildSet = any;
+type TimeoutHandle = any;
+type NoTimeout = any;
 
+// https://medium.com/@agent_hunt/hello-world-custom-react-renderer-9a95b7cd04bc
+const hostConfig: ReactReconciler.HostConfig<Type, Props, Container, Instance, TextInstance, HydratableInstance, PublicInstance, HostContext, UpdatePayload, ChildSet, TimeoutHandle, NoTimeout> = {
+    now: Date.now,
 };
-
-const ReactReconcilerInst = ReactReconciler(hostConfig);
+const ReactReconcilerInst = ReactReconciler<Type, Props, Container, Instance, TextInstance, HydratableInstance, PublicInstance, HostContext, UpdatePayload, ChildSet, TimeoutHandle, NoTimeout>(hostConfig);
 
 export default {
     render: (
-        reactElement, // <App />
-        domElement, // document.getElementById('root')
-        callback // Optional. called after the component App is rendered or updated
+        reactElement: ReactReconciler.ReactNodeList, // <App />
+        domElement: Container, // document.getElementById('root')
+        callback: () => void|null|undefined = () => undefined // Called after the component is rendered or updated
     ) => {
         console.log(arguments);
         // Create a root Container if it doesnt exist
         if (!domElement._rootContainer) {
-        domElement._rootContainer = ReactReconcilerInst.createContainer(domElement, false);
+        domElement._rootContainer = ReactReconcilerInst.createContainer(domElement, false, false);
         }
 
         // update the root Container
-        return ReactReconcilerInst.updateContainer(reactElement, domElement._rootContainer, null, callback);
+        return ReactReconcilerInst.updateContainer(
+            reactElement,
+            domElement._rootContainer,
+            null,
+            callback
+        );
     }
 };
