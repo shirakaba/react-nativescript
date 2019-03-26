90817edb2304446c96dedec87cfff579da11f11f
Jamie Birch work laptop <jbizzle5@gmail.com>
2019-03-26 08:03:11 +0000

    Programmatic app startup (no React yet)

--- sample/app/app.ts
+++ sample/app/app.ts
@@ -6,49 +6,55 @@ purpose of the file is to pass control to the app’s first module.
 
 import * as application from "tns-core-modules/application";
 import * as React from "react";
-import { on, run, launchEvent, getMainEntry, getRootView } from "tns-core-modules/application";
+import { on, run, launchEvent, getMainEntry, getRootView, LaunchEventData } from "tns-core-modules/application";
 import { default as ReactNativeScript } from "react-nativescript/dist/index";
 import { Frame } from "tns-core-modules/ui/frame/frame";
 import { ContentView } from "tns-core-modules/ui/content-view/content-view";
 import { TextBase } from "tns-core-modules/ui/text-base/text-base";
+import { TextField } from "tns-core-modules/ui/text-field/text-field";
 import { Page } from "tns-core-modules/ui/page/page";
+import { FlexboxLayout } from "tns-core-modules/ui/layouts/flexbox-layout/flexbox-layout";
 
 // const contentView = new ContentView();
 // const textBase = new TextBase();
 // textBase.text = "Hello, world!";
 // contentView._addView(textBase);
 
-on(launchEvent, (data: any) => {
-    console.log("[launchEvent 1a] Got launch event. Data:", data);
+run({
+    create: () => {
+        const frame = new Frame();
+        frame.navigate({
+            create: () => {
+                const page = new Page();
+                page.backgroundColor = "green";
 
-    // https://github.com/NativeScript/NativeScript/issues/5073#issuecomment-344703902
-    const frame = new Frame();
-    /* If you don't navigate the frame to a page, a blank UINavigationBar will be inserted. */
-    const page = new Page();
-    page.backgroundColor = "green";
-    frame.navigate({ create: () => page });
-    // https://github.com/NativeScript/NativeScript/blob/9d7f0e5315be0da6574ef1febc80ddb1f5deef32/tns-core-modules/ui/frame/frame.android.ts#L926
-    // frame.navigate(getMainEntry());
+                const contentView = new ContentView();
 
-    data.root = frame; // Should this be the frame or the page?
+                const textField = new TextField();
+                contentView.height = 100;
+                contentView.width = 100;
+                contentView.backgroundColor = "purple";
+                textField.text = "Hello, world!";
 
-    // application.getRootView() and data._window still undefined at this point.
+                contentView.content = textField;
+                page.content = contentView;
 
-    const myTextBase = React.createElement(MyTextBase, { toWhat: 'World' }, null);
+                // // https://reactjs.org/docs/react-without-jsx.html
+                // ReactNativeScript.render(
+                //     React.createElement(MyFlexboxLayout, { flaggy: true }, null),
+                //     page, // I think this is more appropriate than passing frame.
+                //     //@ts-ignore
+                //     // React.createElement(MyRootView, {}, null),
+                //     () => {
+                //         console.log(`Container updated!`);
+                //     }
+                // );
 
-    console.log("myTextBase as React element:", myTextBase);
-    // console.log("myTextBase type:", myTextBase.type);
-
-    // https://reactjs.org/docs/react-without-jsx.html
-    ReactNativeScript.render(
-        React.createElement(MyFlexboxLayout, { flaggy: true }, null),
-        page, // I think this is more appropriate than passing frame.
-        //@ts-ignore
-        // React.createElement(MyRootView, {}, null),
-        () => {
-            console.log(`Container updated!`);
-        }
-    );
+                return page;
+            }
+        });
+        return frame;
+    }
 });
 
 class MyFlexboxLayout extends React.Component<{ flaggy: boolean }, {}> {
