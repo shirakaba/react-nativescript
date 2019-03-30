ec6957b5fbf831777f21a51aa2d83b67443d42b7
Jamie Birch work laptop <jbizzle5@gmail.com>
2019-03-30 17:23:25 +0000

    Backtrack on eventMap idea, but stick with updateListener() function

--- react-nativescript/src/components/eventHandling.ts
+++ react-nativescript/src/components/eventHandling.ts
@@ -1,43 +1,21 @@
-import { Observable } from "tns-core-modules/data/observable";
+import { Observable, EventData } from "tns-core-modules/data/observable";
 
-export interface ListenerEventsMap {
-    [eventName: string]: (...args: any[]) => void;
-}
+// type GenericListener = (...args: any[]) => void;
+type GenericListener = (data: EventData) => void
 
-export function attachListeners<T extends Observable>(node: T, listeners: ListenerEventsMap): void {
-    return attachOrDetachListeners(node, listeners, true);
-}
-
-export function detachListeners<T extends Observable>(node: T, listeners: ListenerEventsMap): void {
-    return attachOrDetachListeners(node, listeners, false);
-}
-
-function attachOrDetachListeners<T extends Observable>(node: T, listeners: ListenerEventsMap, attach: boolean): void {
-    Object.keys(listeners).forEach((name: string) => {
-        const listener = listeners[name];
-        if(!listener) return;
-        attach ? node.on(name, listener) : node.off(name, listener);
-    });
-}
-
-export function updateListeners<T extends Observable>(node: T, oldListeners: ListenerEventsMap, newListeners: ListenerEventsMap): void {
-    Object.keys(oldListeners).forEach((name: string) => {
-        const currentListener = oldListeners[name];
-        const incomingListener = newListeners[name];
-
-        if(currentListener){
-            if(incomingListener){
-                if(incomingListener !== currentListener){
-                    node.off(name, currentListener);
-                    node.on(name, incomingListener);
-                }
-            } else {
-                node.off(name, currentListener);
+export function updateListener<T extends Observable>(node: T, eventName: string, currentListener: GenericListener|undefined, incomingListener: GenericListener|undefined): void {
+    if(currentListener){
+        if(incomingListener){
+            if(incomingListener !== currentListener){
+                node.off(eventName, currentListener);
+                node.on(eventName, incomingListener);
             }
         } else {
-            if(incomingListener){
-                node.on(name, incomingListener);
-            }
+            node.off(eventName, currentListener);
         }
-    });
+    } else {
+        if(incomingListener){
+            node.on(eventName, incomingListener);
+        }
+    }
 }
