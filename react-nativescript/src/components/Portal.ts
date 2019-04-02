9f8de83b97412fd497990350c99e4e2315ed5c5c
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-04-02 10:11:42 +0100

    Working towards getting reference to React tree

---
+++ react-nativescript/src/components/Portal.ts
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { default as ReactNativeScript } from "../index"
+import { ContentView } from "tns-core-modules/ui/content-view/content-view";
+
+// interface Props {
+
+// }
+
+// /**
+//  * Under construction. Just planning to play around with it later.
+//  * https://css-tricks.com/using-react-portals-to-render-children-outside-the-dom-hierarchy/
+//  */
+// // const portalRoot = document.getElementById("portal");
+// const portalRoot = new ContentView();
+// class Portal extends React.Component<Props, {}> {
+//     private readonly el: ContentView = new ContentView();
+//     constructor(props) {
+//       super(props);
+//       // 1: Create a new div that wraps the component
+//     }
+//     // 2: Append the element to the DOM when it mounts
+//     componentDidMount = () => {
+//       portalRoot.content = this.el;
+//     };
+//     // 3: Remove the element when it unmounts
+//     componentWillUnmount = () => {
+//       portalRoot.content = null;
+//     };
+//     render() {
+//       // 4: Render the element's children in a Portal
+//       const { children } = this.props;
+//       return ReactNativeScript.createPortal(children, this.el);
+//     }
+// }
