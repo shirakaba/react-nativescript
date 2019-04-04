5c386e659eac1a32f6e9fc886961bb76ef6d9e0b
Jamie Birch work laptop <jbizzle5@gmail.com>
2019-04-04 20:45:12 +0100

    Getting closer to plausible ListView implementation, but Portal not working.

---
+++ react-nativescript/src/client/ReactPortal.ts
@@ -0,0 +1,27 @@
+import { ReactNodeList } from "react-reconciler";
+import { ReactPortal } from "react";
+
+/**
+ * Code in here referenced from: https://github.com/facebook/react/blob/b87aabdfe1b7461e7331abb3601d9e6bb27544bc/packages/shared/ReactPortal.js which carries the following copyright:
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in React-LICENSE.txt.
+ */
+
+export function createPortal(
+  children: ReactNodeList,
+  containerInfo: any,
+  // TODO: figure out the API for cross-renderer implementation.
+  implementation: any|null,
+  key: string|null = null,
+): ReactPortal {
+  return {
+    // This tag allow us to uniquely identify this as a React Portal
+    //@ts-ignore
+    $$typeof: (global as any).REACT_PORTAL_TYPE,
+    key: key == null ? null : '' + key,
+    children,
+    containerInfo,
+    implementation,
+  };
+}
