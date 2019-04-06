63b6f925cd1b59282464b943029e6102f17bc04f
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-04-06 21:18:31 +0100

    Reduce logging verbosity

--- sample/app/app.ts
+++ sample/app/app.ts
@@ -31,7 +31,7 @@ run({
                         {
                             items: [
                                 /* Enough cells to see how view recycling works/ doesn't work */
-                                ...[...Array(50).keys()].map(x => x + 1).map((val) => {
+                                ...[...Array(50).keys()].map((val) => {
                                     return { text: val };
                                 })
                             ]
