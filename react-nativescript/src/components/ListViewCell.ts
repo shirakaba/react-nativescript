a4b54f903282547bc3923db18e44aab2aa246837
Jamie Birch work laptop <jbizzle5@gmail.com>
2019-04-07 10:23:52 +0100

    more confirmation that props are passing correctly but re-render simply isn't occurring.

--- react-nativescript/src/components/ListViewCell.ts
+++ react-nativescript/src/components/ListViewCell.ts
@@ -3,27 +3,30 @@ import { default as ReactNativeScript } from "../index";
 import { ContentView } from "tns-core-modules/ui/page/page";
 
 interface Props {
+    identifier: string,
+    textReference: string,
     nativeElement: ContentView
 }
 
 export class ListViewCell extends React.Component<Props, {}> {
     componentDidMount(){
-        console.log(`[ListViewCell] componentDidMount!`);
+        console.log(`[ListViewCell ${this.props.identifier}] componentDidMount! With: ${this.props.textReference}`);
     }
 
     shouldComponentUpdate(nextProps: Props, nextState: {}): boolean {
-        console.log(`[ListViewCell] shouldComponentUpdate!`);
+        console.log(`[ListViewCell ${this.props.identifier}] shouldComponentUpdate! With: ${nextProps.textReference}`);
         return true;
     }
 
     componentWillUnmount(){
-        console.log(`[ListViewCell] componentWillUnmount!`);
+        console.log(`[ListViewCell ${this.props.identifier}] componentWillUnmount!`);
     }
 
     render(){
         return ReactNativeScript.createPortal(
             this.props.children,
-            this.props.nativeElement
+            this.props.nativeElement,
+            this.props.identifier
         );
     }
 }
