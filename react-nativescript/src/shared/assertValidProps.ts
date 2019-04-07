bb3ed85240c381f181196d2913210ada712f56c9
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-04-07 17:31:48 +0100

    ReactNativeScriptComponent and assertValidProps copied over from reactDOM

---
+++ react-nativescript/src/shared/assertValidProps.ts
@@ -0,0 +1,38 @@
+/**
+ * Code in here referenced from: https://github.com/facebook/react/blob/master/packages/react-dom/src/shared/assertValidProps.js which carries the following copyright:
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in React-LICENSE.txt.
+ */
+
+function assertValidProps(tag: string, props?: any): void|never {
+    if (!props) {
+        return;
+    }
+
+    if (__DEV__) {
+        if(
+            !(
+                props.suppressContentEditableWarning ||
+                !props.contentEditable ||
+                props.children == null
+            )
+        ){
+            console.warn(
+                'A component is `contentEditable` and contains `children` managed by ' +
+                    'React. It is now your responsibility to guarantee that none of ' +
+                    'those nodes are unexpectedly modified or duplicated. This is ' +
+                    'probably not intentional.',
+            )
+        }
+    }
+
+    if(!(props.style == null || typeof props.style === 'object')){
+        throw new Error(
+            'The `style` prop expects a mapping from style properties to values, ' +
+            "not a string. For example, style={{marginRight: spacing + 'em'}} when " +
+            'using JSX.',
+        );
+    }
+}
+export default assertValidProps;
