7fcb4c377d5d8efd56cfde75db634923a1f38648
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-05-08 22:50:17 +0100

    GridLayout extends LayoutBase

--- react-nativescript/src/components/GridLayout.ts
+++ react-nativescript/src/components/GridLayout.ts
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { GridLayoutProps } from "../shared/NativeScriptComponentTypings";
 import { GridLayout as NativeScriptGridLayout, ItemSpec } from "tns-core-modules/ui/layouts/grid-layout/grid-layout";
+import { GridLayoutProps, PropsWithoutForwardedRef } from "../shared/NativeScriptComponentTypings";
+import { LayoutBaseComponentProps, RCTLayoutBase } from "./LayoutBase";
 
 interface Props {
     /* Each constituent view passed in must bear "row" and "col" Props. */
@@ -10,25 +11,63 @@ interface Props {
     // TODO: Should we enforce that children must only be Views?
 }
 
-export type GridLayoutComponentProps = Props & Partial<GridLayoutProps>;
+export type GridLayoutComponentProps<E extends NativeScriptGridLayout = NativeScriptGridLayout> = Props /* & typeof RCTGridLayout.defaultProps */ & Partial<GridLayoutProps> & LayoutBaseComponentProps<E>;
 
 /**
  * A React wrapper around the NativeScript GridLayout component.
  * See: ui/layouts/grid-layout
  */
-export class GridLayout extends React.Component<GridLayoutComponentProps, {}> {
-    private readonly myRef: React.RefObject<NativeScriptGridLayout> = React.createRef<NativeScriptGridLayout>();
-
+export class _GridLayout<P extends GridLayoutComponentProps<E>, S extends {}, E extends NativeScriptGridLayout> extends RCTLayoutBase<P, S, E> {
     render(){
-        const { children, ...rest } = this.props;
+        const {
+            forwardedRef,
+
+            onLoaded,
+            onUnloaded,
+            onAndroidBackPressed,
+            onShowingModally,
+            onShownModally,
+            
+            onTap,
+            onDoubleTap,
+            onPinch,
+            onPan,
+            onSwipe,
+            onRotation,
+            onLongPress,
+            onTouch,
+
+            onPropertyChange,
+            
+            children,
+
+            ...rest
+        } = this.props;
 
         return React.createElement(
             'gridLayout',
             {
                 ...rest,
-                ref: this.myRef
+                ref: forwardedRef || this.myRef
             },
             children
         );
     }
 }
+
+type OwnPropsWithoutForwardedRef = PropsWithoutForwardedRef<GridLayoutComponentProps<NativeScriptGridLayout>>;
+
+export const GridLayout: React.ComponentType<OwnPropsWithoutForwardedRef & React.ClassAttributes<NativeScriptGridLayout>> = React.forwardRef<NativeScriptGridLayout, OwnPropsWithoutForwardedRef>(
+    (props: React.PropsWithChildren<OwnPropsWithoutForwardedRef>, ref: React.RefObject<NativeScriptGridLayout>) => {
+        const { children, ...rest } = props;
+
+        return React.createElement(
+            _GridLayout,
+            {
+                ...rest,
+                forwardedRef: ref,
+            },
+            children
+        );
+    }
+)
