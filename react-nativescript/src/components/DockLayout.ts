e04b152d48eddf367e5e1739eb2be139b456910f
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-05-08 22:47:52 +0100

    DockLayout extends LayoutBase

--- react-nativescript/src/components/DockLayout.ts
+++ react-nativescript/src/components/DockLayout.ts
@@ -1,31 +1,70 @@
 import * as React from "react";
-import { DockLayoutProps } from "../shared/NativeScriptComponentTypings";
+import { DockLayoutProps, PropsWithoutForwardedRef } from "../shared/NativeScriptComponentTypings";
 import { DockLayout as NativeScriptDockLayout } from "tns-core-modules/ui/layouts/dock-layout/dock-layout";
+import { LayoutBaseComponentProps, RCTLayoutBase } from "./LayoutBase";
 
 interface Props {
     // No mandatory props.
     /* Each constituent view passed in must bear a "dock" Prop. */
 }
 
-export type DockLayoutComponentProps = Props & Partial<DockLayoutProps>;
+export type DockLayoutComponentProps<E extends NativeScriptDockLayout = NativeScriptDockLayout> = Props /* & typeof RCTDockLayout.defaultProps */ & Partial<DockLayoutProps> & LayoutBaseComponentProps<E>;
 
 /**
  * A React wrapper around the NativeScript DockLayout component.
  * See: ui/layouts/dock-layout
  */
-export class DockLayout extends React.Component<DockLayoutComponentProps, {}> {
-    private readonly myRef: React.RefObject<NativeScriptDockLayout> = React.createRef<NativeScriptDockLayout>();
-
+export class _DockLayout<P extends DockLayoutComponentProps<E>, S extends {}, E extends NativeScriptDockLayout> extends RCTLayoutBase<P, S, E> {
     render(){
-        const { children, ...rest } = this.props;
+        const {
+            forwardedRef,
+
+            onLoaded,
+            onUnloaded,
+            onAndroidBackPressed,
+            onShowingModally,
+            onShownModally,
+            
+            onTap,
+            onDoubleTap,
+            onPinch,
+            onPan,
+            onSwipe,
+            onRotation,
+            onLongPress,
+            onTouch,
+
+            onPropertyChange,
+            
+            children,
+
+            ...rest
+        } = this.props;
 
         return React.createElement(
             'dockLayout',
             {
                 ...rest,
-                ref: this.myRef
+                ref: forwardedRef || this.myRef
             },
             children
         );
     }
 }
+
+type OwnPropsWithoutForwardedRef = PropsWithoutForwardedRef<DockLayoutComponentProps<NativeScriptDockLayout>>;
+
+export const DockLayout: React.ComponentType<OwnPropsWithoutForwardedRef & React.ClassAttributes<NativeScriptDockLayout>> = React.forwardRef<NativeScriptDockLayout, OwnPropsWithoutForwardedRef>(
+    (props: React.PropsWithChildren<OwnPropsWithoutForwardedRef>, ref: React.RefObject<NativeScriptDockLayout>) => {
+        const { children, ...rest } = props;
+
+        return React.createElement(
+            _DockLayout,
+            {
+                ...rest,
+                forwardedRef: ref,
+            },
+            children
+        );
+    }
+)
