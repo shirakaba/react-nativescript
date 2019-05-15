eda6422e78a1d512b0c74838929fbfef67d03c23
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-05-15 09:28:28 +0100

    Notes on Progress component

--- react-nativescript/src/components/Progress.ts
+++ react-nativescript/src/components/Progress.ts
@@ -2,8 +2,9 @@ import * as React from "react";
 import { ProgressProps, PropsWithoutForwardedRef } from "../shared/NativeScriptComponentTypings";
 import { Progress as NativeScriptProgress } from "tns-core-modules/ui/progress/progress";
 import { ViewComponentProps, RCTView } from "./View";
+// import { Observable } from "../client/ElementRegistry";
+// import { EventData } from "tns-core-modules/data/observable/observable";
 
-// TODO: refer to https://docs.nativescript.org/ui/ns-ui-widgets/progress for API
 interface Props {
 }
 
@@ -13,6 +14,25 @@ export class _Progress<P extends ProgressComponentProps<E>, S extends {}, E exte
     // static defaultProps = {
     //     forwardedRef: React.createRef<NativeScriptProgress>()
     // };
+
+    /* Not sure there's really a use-case for Observable here, given that we're using React.
+    * Was replicating: https://docs.nativescript.org/ui/ns-ui-widgets/progress */
+    // private readonly vm = new Observable();
+    // componentDidMount(){
+    //     super.componentDidMount();
+
+    //     this.vm.set("prResult", this.props.value);
+
+    //     const ref = this.props.forwardedRef || this.myRef;
+    //     const node: E|null = ref.current;
+    //     if(node){
+    //         node.on("valueChange", (pargs: EventData) => {
+    //             this.vm.set("prResult", (<NativeScriptProgress>pargs.object).value);
+    //         });
+    //     } else {
+    //         console.warn(`React ref to NativeScript View lost, so unable to update event listeners.`);
+    //     }
+    // }
 
     render(): React.ReactNode {
         const {
