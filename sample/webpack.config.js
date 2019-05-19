873c0f3a3dea999315cf6b83063db22fd071f25f
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-05-19 19:27:13 +0100

    Try introducing the other babel plugin

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -22,7 +22,7 @@ const babelLoader = {
         plugins: [
             /* plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript */
             // ["@babel/plugin-proposal-decorators", { legacy: true }],
-            // ["@babel/plugin-proposal-class-properties", { loose: true }],
+            ["@babel/plugin-proposal-class-properties", { loose: true }],
             "react-hot-loader/babel"
         ]
     }
