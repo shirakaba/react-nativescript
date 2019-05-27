90fe83ac83fe664d5d8df57685f5c0569b7a2a92
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-05-27 11:43:13 +0100

    Simplified startup

--- sample/app/app.ts
+++ sample/app/app.ts
@@ -21,79 +21,10 @@ if(module.hot){
 }
 
 import * as React from "react";
-import * as application from "tns-core-modules/application";
-import { run, hasLaunched, getRootView } from "tns-core-modules/application";
 import * as ReactNativeScript from "react-nativescript/dist/index";
-import { render } from "react-nativescript/dist/index";
-import { RCTContentView, RCTLabel } from "react-nativescript/dist/index";
-import { FormattedStringLabel } from "./testComponents/testComponents";
-import { GestureLoggingTest, PanGestureTest, PageGestureTest, StatefulPageGestureTest, StatefulPageGestureTest2 } from "./testComponents/gestures";
-import { GameLoopTest } from "./testComponents/stateful";
-import { NestedHub, NestedModalTest, HubTest, SimpleHub, ActionBarTest, TabViewTest } from "./testComponents/navigation";
-import { Frame, Page, StackLayout, ProxyViewContainer, ContentView, View, TabView } from "react-nativescript/dist/client/ElementRegistry";
-import { SpriteKitGameTest } from "./testComponents/spriteKitGame";
-import { ListViewTest, DynamicListViewWithImages } from "./testComponents/list";
-import HotApp from "./testComponents/HotApp";
+import HotApp, { rootRef } from "./testComponents/HotApp";
 
-// ReactNativeScript.startWithView(
-//     React.createElement(
-//         HotApp,
-//         {
-            
-//         },
-//         null
-//     ),
-//     new ContentView()
-// );
-
-const appRef = React.createRef<Frame>();
-/* Do any Frame setup here */
-ReactNativeScript.startWithAnyView(
-    React.createElement(
-        HotApp,
-        {
-            forwardedRef: appRef
-        },
-        null
-    ),
-    appRef
-);
-
-
-// console.log("BOO HISS OY EY");
-
-// const frame = new Frame();
-// const page = new Page();
-// // page.actionBarHidden = false;
-// ReactNativeScript.startWithFrameAndPage(
-//     React.createElement(
-//         HubTest,
-//         {
-//             forwardedRef: pageRef
-//         },
-//         null
-//     ),
-//     frame,
-//     page
-// );
-
-// const pageRef = React.createRef<Page>();
-// const frame = new Frame();
-// /* Do any Frame setup here */
-// ReactNativeScript.startWithFrame(
-//     React.createElement(
-//         StatefulPageGestureTest,
-//         {
-//             forwardedRef: pageRef
-//         },
-//         null
-//     ),
-//     frame,
-//     pageRef
-// );
-
-
-// application.run({ moduleName: "app-root" });
+ReactNativeScript.startWithAnyView(React.createElement(HotApp, {}, null), rootRef);
 
 /*
 Do not place any code after the application has been started as it will not
--- sample/app/testComponents/hotApp.tsx
+++ sample/app/testComponents/hotApp.tsx
@@ -11,9 +11,16 @@
 import { hot } from 'react-hot-loader';
 import * as React from "react";
 
-import { DynamicListViewWithImages } from "./list";
+import { Frame, Page, StackLayout, ProxyViewContainer, ContentView, View, TabView } from "react-nativescript/dist/client/ElementRegistry";
 import { DockLayoutTest, HMRTest } from './layout';
-import { FrameTest } from './navigation';
+import { FormattedStringLabel } from "./testComponents";
+import { GestureLoggingTest, PanGestureTest, PageGestureTest, StatefulPageGestureTest, StatefulPageGestureTest2 } from "./gestures";
+import { GameLoopTest } from "./stateful";
+import { NestedHub, NestedModalTest, HubTest, SimpleHub, ActionBarTest, TabViewTest, FrameTest } from "./navigation";
+import { SpriteKitGameTest } from "./spriteKitGame";
+import { ListViewTest, DynamicListViewWithImages } from "./list";
 
-// export default HMRTest; /* */
-export default hot(module)(FrameTest); /* */
+export const rootRef = React.createRef<any>();
+const app = () => <HubTest forwardedRef={rootRef}/>
+
+export default hot(module)(app);
