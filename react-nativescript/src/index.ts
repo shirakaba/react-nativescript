85deacfbe50a5d4f11c140474d7c367f029e5122
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-06-22 22:50:18 +0100

    Looks like module.d.ts wasn't doing anything after all

--- react-nativescript/src/index.ts
+++ react-nativescript/src/index.ts
@@ -1,6 +1,1 @@
-/// <reference path="../module.d.ts" />
-// import * as ReactNativeScript from "./client/ReactNativeScript";
-
-// export default ReactNativeScript;
-// export { default } from "./client/ReactNativeScript";
 export * from "./client/ReactNativeScript";
