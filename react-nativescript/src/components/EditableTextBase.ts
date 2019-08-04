4252e43bddb4c6a67e59c088352004ca4db1f903
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-08-04 20:38:28 +0100

    "blur", "focus", and "textChange" events on EditableTextBase

--- react-nativescript/src/components/EditableTextBase.ts
+++ react-nativescript/src/components/EditableTextBase.ts
@@ -3,8 +3,14 @@ import * as React from "react";
 import { EditableTextBase as NativeScriptEditableTextBase } from "../client/ElementRegistry";
 import { EditableTextBaseProps } from "../shared/NativeScriptComponentTypings";
 import { RCTTextBase, TextBaseComponentProps } from "./TextBase";
+import { EventData } from "tns-core-modules/ui/text-base/text-base";
+import { updateListener } from "../client/EventHandling";
 
-interface Props {}
+interface Props {
+	onBlur?: (args: EventData) => void;
+	onFocus?: (args: EventData) => void;
+    onTextChange?: (args: EventData) => void;
+}
 
 export type EditableTextBaseComponentProps<
     E extends NativeScriptEditableTextBase = NativeScriptEditableTextBase
@@ -14,4 +20,23 @@ export abstract class RCTEditableTextBase<
     P extends EditableTextBaseComponentProps<E>,
     S extends {},
     E extends NativeScriptEditableTextBase
-> extends RCTTextBase<P, S, E> {}
+> extends RCTTextBase<P, S, E> {
+    /**
+     *
+     * @param attach true: attach; false: detach; null: update
+     */
+    protected updateListeners(node: E, attach: boolean | null, nextProps?: P): void {
+        super.updateListeners(node, attach, nextProps);
+
+        if (attach === null) {
+            updateListener(node, "blur", this.props.onBlur, nextProps.onBlur);
+            updateListener(node, "focus", this.props.onFocus, nextProps.onFocus);
+            updateListener(node, "textChange", this.props.onTextChange, nextProps.onTextChange);
+        } else {
+            const method = (attach ? node.on : node.off).bind(node);
+            if (this.props.onBlur) method("blur", this.props.onBlur);
+            if (this.props.onFocus) method("focus", this.props.onFocus);
+            if (this.props.onTextChange) method("textChange", this.props.onTextChange);
+        }
+    }
+}
--- react-nativescript/src/components/TextBase.ts
+++ react-nativescript/src/components/TextBase.ts
@@ -3,12 +3,8 @@ import * as React from "react";
 import { TextBase as NativeScriptTextBase } from "../client/ElementRegistry";
 import { ContentViewProps, TextBaseProps } from "../shared/NativeScriptComponentTypings";
 import { ViewComponentProps, RCTView } from "./View";
-import { updateListener } from "../client/EventHandling";
-import { EventData } from "tns-core-modules/ui/text-base/text-base";
 
 interface Props {
-    // https://docs.nativescript.org/ui/ns-ui-widgets/text-field#code-behind
-    onTextChange?: (args: EventData) => void;
 }
 
 export type TextBaseComponentProps<
@@ -20,18 +16,4 @@ export abstract class RCTTextBase<
     S extends {},
     E extends NativeScriptTextBase
 > extends RCTView<P, S, E> {
-    /**
-     *
-     * @param attach true: attach; false: detach; null: update
-     */
-    protected updateListeners(node: E, attach: boolean | null, nextProps?: P): void {
-        super.updateListeners(node, attach, nextProps);
-
-        if (attach === null) {
-            updateListener(node, "textChange", this.props.onTextChange, nextProps.onTextChange);
-        } else {
-            const method = (attach ? node.on : node.off).bind(node);
-            if (this.props.onTextChange) method("textChange", this.props.onTextChange);
-        }
-    }
 }
