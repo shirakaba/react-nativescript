0902bf3e0a73c89096924decfad5e3c2846ffa43
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-11-26 22:07:58 +0000

    Page now based on hooks

--- react-nativescript/src/components/Page.ts
+++ react-nativescript/src/components/Page.ts
@@ -17,7 +17,7 @@ export type PageNavigationEventHandler = (args: NavigatedData) => void;
 
 export type PageComponentProps<
     E extends NativeScriptPage = NativeScriptPage
-> = Props /* & typeof _Page.defaultProps */ & Partial<PageProps> & ContentViewComponentProps<E>;
+> = Props & Partial<PageProps> & ContentViewComponentProps<E>;
 
 /**
  * A React wrapper around the NativeScript Page component.
@@ -28,56 +28,21 @@ export function _Page<
     E extends NativeScriptPage = NativeScriptPage
 >(props: React.PropsWithChildren<P>)
 {
-    const {
-        forwardedRef,
-
-        onNavigatedFrom,
-        onNavigatedTo,
-        onNavigatingFrom,
-        onNavigatingTo,
-
-        onLoaded,
-        onUnloaded,
-        onAndroidBackPressed,
-        onShowingModally,
-        onShownModally,
-
-        onTap,
-        onDoubleTap,
-        onPinch,
-        onPan,
-        onSwipe,
-        onRotation,
-        onLongPress,
-        onTouch,
-
-        onPropertyChange,
-
-        children,
-        ...rest
-    } = props;
-
-    const ref: React.RefObject<E> = useRef();
-    const node: E = ref.current!;
-
-    usePageInheritance(node, props);
+    const ref: React.RefObject<E> = (this.props.forwardedRef || useRef());
+    const intrinsicProps = usePageInheritance(ref.current!, props);
 
     return React.createElement(
         "page",
         {
-            ...rest,
-            ref: forwardedRef || ref,
+            ...intrinsicProps,
+            ref,
         },
-        children
+        null
     );
 }
 
-type OwnPropsWithoutForwardedRef = PropsWithoutForwardedRef<PageComponentProps<NativeScriptPage>>;
-
-export const Page: React.ComponentType<
-    OwnPropsWithoutForwardedRef & React.ClassAttributes<NativeScriptPage>
-> = React.forwardRef<NativeScriptPage, OwnPropsWithoutForwardedRef>(
-    (props: React.PropsWithChildren<OwnPropsWithoutForwardedRef>, ref: React.RefObject<NativeScriptPage>) => {
+export const Page = React.forwardRef<NativeScriptPage, PropsWithoutForwardedRef<PageComponentProps>>(
+    (props: React.PropsWithChildren<PropsWithoutForwardedRef<PageComponentProps>>, ref: React.RefObject<NativeScriptPage>) => {
         const { children, ...rest } = props;
 
         return React.createElement(
@@ -91,6 +56,13 @@ export const Page: React.ComponentType<
     }
 );
 
+/**
+ * A hook to handle adding/removing events any time a dependent event listener handler in the props changes value.
+ * That is to say, on mount, update, and unmount.
+ * 
+ * @param node the host instance of the underlying intrinsic element for this React component.
+ * @param props the props for the React component (from which this function will use any event listener handlers).
+ */
 export function usePageEvents<
     P extends PageComponentProps<E>,
     E extends NativeScriptPage = NativeScriptPage
@@ -105,14 +77,34 @@ export function usePageEvents<
     useEventListener(node, "navigatingTo", props.onNavigatingTo);
 }
 
+/**
+ * A hook to inherit all the behaviour of this React component. Useful when creating a React component that
+ * wraps an intrinsic element that extends the same intrinsic element as this one.
+ * 
+ * @param node the host instance of the underlying intrinsic element for this React component.
+ * @param props all props for the intrinsic element and also its React wrapper (e.g. event listener handlers).
+ * 
+ * @returns just the props to be passed on to the underlying intrinsic element.
+ */
 export function usePageInheritance<
     P extends PageComponentProps<E>,
     E extends NativeScriptPage = NativeScriptPage
 >(
     node: E,
     props: P
-): void
+)
 {
-    useContentViewInheritance(node, props);
-    usePageEvents(node, props);
+    const intrinsicProps = useContentViewInheritance(node, props);
+    usePageEvents(node, intrinsicProps);
+
+    const {
+        onNavigatedFrom,
+        onNavigatedTo,
+        onNavigatingFrom,
+        onNavigatingTo,
+        ...rest
+    } = intrinsicProps;
+
+    // Omit all event handlers because they aren't used by the intrinsic element.
+    return { ...rest };
 }
