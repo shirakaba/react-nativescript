73b102c3340e77432ff9bfaa33fb4d7a59d876bf
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-06-12 19:21:24 +0100

    Sooo fast!

--- sample/react-nativescript.webpack.config.js
+++ sample/react-nativescript.webpack.config.js
@@ -1,6 +1,7 @@
 const webpackConfig = require("./webpack.config");
 const webpack = require("webpack");
 const path = require("path");
+const util = require('util')
 
 module.exports = (env) => {
     env = env || {};
@@ -33,22 +34,43 @@ module.exports = (env) => {
 
     // Remove ts-loader as we'll be using Babel to transpile the TypeScript instead.
     baseConfig.module.rules = baseConfig.module.rules.filter((rule) => {
-        return rule.loader !== "ts-loader";
+        const isTsLoader = rule.use && rule.use.loader === "ts-loader";
+        return !isTsLoader;
+    });
+
+    // Modify "nativescript-dev-webpack/hmr/hot-loader" to test for .tsx files
+    // (and also js(x) files, which it should have been doing to begin with!)
+    baseConfig.module.rules.some(rule => {
+        const isNativeScriptDevWebpackHotLoader = rule.use === "nativescript-dev-webpack/hmr/hot-loader";
+
+        if(isNativeScriptDevWebpackHotLoader){
+            rule.test = /\.(ts|tsx|js|jsx|css|scss|html|xml)$/;
+        }
+
+        return isNativeScriptDevWebpackHotLoader; // Break loop once we've found the one.
     });
 
     baseConfig.module.rules.push(
         {
-            test: /\.ts(x?)$/,
+            test: /\.[jt]s(x?)$/,
             exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: babelOptions
-            },
+            use: [
+                {
+                    loader: "babel-loader",
+                    options: babelOptions
+                }
+            ],
         }
     );
 
     baseConfig.resolve.extensions = [".ts", ".tsx", ".js", ".jsx", ".scss", ".css"];
     baseConfig.resolve.alias["react-dom"] = "react-nativescript";
+
+    // Remove ForkTsCheckerWebpackPlugin because, now that we're using Babel, we'll leave type-checking to the IDE instead.
+    baseConfig.plugins = baseConfig.plugins.filter(plugin => {
+        const isForkTsCheckerWebpackPlugin = plugin && plugin.constructor && plugin.constructor.name === "ForkTsCheckerWebpackPlugin";
+        return !isForkTsCheckerWebpackPlugin;
+    });
 
     // Augment NativeScript's existing DefinePlugin definitions with a few more of our own.
     let existingDefinePlugin;
@@ -57,29 +79,26 @@ module.exports = (env) => {
         existingDefinePlugin = plugin;
         return !isDefinePlugin;
     });
-    baseConfig.plugins.unshift(
-        new webpack.DefinePlugin({
-            ...existingDefinePlugin.definitions,
-            /* For various libraries in the React ecosystem. */
-            "__DEV__": production ? "false" : "true",
-            "__TEST__": "false",
-            ...(
-                hmr ?
-                    {
-                        /* react-hot-loader expects to run in an environment where globals are stored on the `window` object.
-                        * NativeScript uses `global` instead, so we'll alias that to satisfy it.
-                        *
-                        * Somehow `var globalValue = window` throws a ReferenceError if `window` is aliased to `global`,
-                        * but is fine if aliased to `global.global`. */
-                        "window": "global.global",
-                        /* Stops react-hot-loader from being bundled in production mode:
-                        * https://github.com/gaearon/react-hot-loader/issues/602#issuecomment-340246945 */
-                        "process.env.NODE_ENV": JSON.stringify(production ? "production" : "development"),
-                    } :
-                    {}
-            ),
-        }),
-    );
+    const newDefinitions = {
+        ...existingDefinePlugin.definitions,
+        /* For various libraries in the React ecosystem. */
+        "__DEV__": production ? "false" : "true",
+        "__TEST__": "false",
+        ...(
+            hmr ?
+                {
+                    /* react-hot-loader expects to run in an environment where globals are stored on the `window` object.
+                     * NativeScript uses `global` instead, so we'll alias that to satisfy it.
+                     *
+                     * Somehow `var globalValue = window` throws a ReferenceError if `window` is aliased to `global`,
+                     * but is fine if aliased to `global.global`. */
+                    "window": "global.global",
+                    "process.env.NODE_ENV": JSON.stringify(production ? "production" : "development"),
+                } :
+                {}
+        ),
+    };
+    baseConfig.plugins.unshift(new webpack.DefinePlugin(newDefinitions));
     
     if(production){
         /* Running this line seems to lead to React Hot Loader bundling production.min.js (which is empty)
@@ -87,6 +106,11 @@ module.exports = (env) => {
          * So it should be understood that HMR is not supported in production mode. */
         baseConfig.plugins = baseConfig.plugins.filter(p => !(p && p.constructor && p.constructor.name === "HotModuleReplacementPlugin"));
     }
+
+    console.log("baseConfig:", util.inspect(baseConfig, {showHidden: false, depth: null, colors: true }));
+    console.log("All env variables:", env);
+    console.log("--hmr", hmr);
+    console.log("--production", production);
 
     return baseConfig;
 };
