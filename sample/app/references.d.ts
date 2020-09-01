08123c934f38f75502e2aaf61e9ed3fbc862649f
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-09-02 00:02:08 +0100

    fix: Add and fix references.d.ts

---
+++ sample/app/references.d.ts
@@ -0,0 +1,3 @@
+//// <reference types="tns-platform-declarations/ios" />
+/// <reference path="../node_modules/@nativescript/types-ios/index.d.ts" />
+/// <reference path="../node_modules/@nativescript/types-android/index.d.ts" />
