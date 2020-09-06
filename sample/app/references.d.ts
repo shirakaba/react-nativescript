08b1dd257ae1102ba389178069426472f2c2ca09
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-09-06 21:35:48 +0100

    fix: Get build to run

--- sample/app/references.d.ts
+++
@@ -1,3 +0,0 @@
-//// <reference types="tns-platform-declarations/ios" />
-/// <reference path="../node_modules/@nativescript/types-ios/index.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-android/index.d.ts" />
